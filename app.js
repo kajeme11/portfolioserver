b2475bef3a54c66dc144e329d5ee09ed615b98a3
kajeme11 <chenequek@gmail.com>
2024-12-05 20:04:04 -0800

    added default port to server, and vercel directional configuration

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(port, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
     
